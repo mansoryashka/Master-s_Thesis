fix FEM formula for 3D beam

3DBeam/FemBeam3D.py
@@ -38,9 +38,15 @@ def FEM_3D(N):
     # B = F * F.T
     # C = F.T * F
     # I1 = dolfin.tr(C)
+
+    F = dolfin.grad(u) + dolfin.Identity(3)
+    J = dolfin.det(F)
+    C = F.T * F
+    I1 = dolfin.tr(C)
     # E = 0.5 * (C - dolfin.Identity(3))
 
     f = dolfin.Constant((0.0, -5.0, 0.0))
+    Fb = J * dolfin.inv(F).T * f        # body force in reference configuration
     # f2 = J*dolfin.inv(F).T*f
 
     # # define epsilon and sigma
@@ -71,22 +77,17 @@ def FEM_3D(N):
     # dolfin.solve(J2 == L, u, bcs=bc,
     #             solver_parameters={"linear_solver": "mumps"})
 
-    F = dolfin.grad(u) + dolfin.Identity(3)
-    J = dolfin.det(F)
-    C = F.T * F
-    I1 = dolfin.tr(C)
-
     psi = 0.5*lmbd*dolfin.ln(J)**2 - mu*dolfin.ln(J) + 0.5*mu*(I1 - 3)
-    energy = psi*dolfin.dx(domain=mesh) #- dolfin.dot(f, u)*ds(1)
+    energy = psi*dolfin.dx(domain=mesh) - dolfin.dot(Fb, u)*ds(1)
     total_internal_work = dolfin.derivative(energy, u, v)
-    total_virtual_work = total_internal_work - dolfin.inner(f, v)*ds(1)
+    total_virtual_work = total_internal_work #- dolfin.inner(f, v)*ds(1)
 
     dolfin.solve(total_virtual_work == 0, u, bc,
                 solver_parameters={'newton_solver': {
                                     # 'absolute_tolerance': 1e-6,
                                     'linear_solver': 'mumps'}})
 
-    # dolfin.File('output/3dbeam_sig.pvd') << u
+    dolfin.File('output/FEMBeam3D.pvd') << u
     # dolfin.File('output/3dbeam_sig2.pvd') << u
 
     x = np.linspace(0, l, 4*N_test+2)[1:-1]
@@ -99,9 +100,10 @@ def FEM_3D(N):
             for k in range(N_test):
                 u_fem[:, i, j, k] = u(x[i], y[j], z[k])
 
-    np.save(f'stored_arrays/u_fem_N={N}', u_fem)
+    np.save(f'stored_arrays/u_fem_N{N}', u_fem)
 
 if __name__ == '__main__':
     for N in [5, 10, 15, 20, 25, 30]:
+    # for N in [20]:
         print('N = ', N)
         FEM_3D(N)

3DBeam/compare_fem.py
@@ -10,15 +10,16 @@ from DEM import L2norm3D
 e = []
 
 u_ref = np.load('stored_arrays/u_fem_N30.npy')
-L2_ref = L2norm3D(u_ref, 4*30, 30, 30, 1/120, 1/30, 1/30)
-
-for N in range(5, 26, 5):
-    u_fem = np.load(f'stored_arrays/u_fem_N{N}.npy')
-    L2_i = L2norm3D(u_fem, 4*30, 30, 30, 1/120, 1/30, 1/30)
-
-    e.append((L2_i - L2_ref)/L2_ref)
+# L2_ref = L2norm3D(u_ref, 4*30, 30, 30, 1/120, 1/30, 1/30)
 
 Ns = [5, 10, 15, 20, 25]
+for N in Ns:
+    u_fem = np.load(f'stored_arrays/u_fem_N{N}.npy')
+    e_i = L2norm3D(u_fem - u_ref, 4*30, 30, 30, 1/120, 1/30, 1/30)
+    # L2_i = L2norm3D(u_fem, 4*30, 30, 30, 1/120, 1/30, 1/30)
+    # print(e_i)
+    e.append(e_i)
+
 for i in range(1, len(e)):
     e_new, e_old = e[i], e[i-1]
     h_new, h_old = 1/Ns[i], 1/Ns[i-1]