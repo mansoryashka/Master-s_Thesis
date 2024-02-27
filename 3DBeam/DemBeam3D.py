ab70c51c4090de8d2159fb6ef2f7e5eb413cb59d
Mansur Dudajev <mansurd@ml1.hpc.uio.no>
2024-02-27 12:24:29 +0100

    change DEM model

--- 3DBeam/DemBeam3D.py
+++ 3DBeam/DemBeam3D.py
@@ -305,7 +305,7 @@ def plot_losses(losses, parameter1, parameter2, dim1, dim2, num_epochs, title, f
     fig.savefig(figures_path / Path(filename + '.pdf'))
 
 if __name__ == '__main__':
-    u_fem30 = np.load('stored_arrays/u_fem_N30.npy')
+    u_fem30 = np.load('stored_arrays/u_fem_N302.npy')
     print(f'FEM: {L2norm3D(u_fem30, 4*N_test, N_test, N_test, dx, dy, dz)} \n')
 
     x = np.linspace(0, L, 4*N_test + 2)[1:-1]
@@ -317,49 +317,48 @@ if __name__ == '__main__':
     # print(U_norms)
     # print((U_norms - L2norm3D(u_fem30, 4*N_test, N_test, N_test, dx, dy, dz))/L2norm3D(u_fem30, 4*N_test, N_test, N_test, dx, dy, dz))
 
-    N = 30
-    lr = .5
-    num_layers = [2, 3, 4, 5]
-    num_neurons = [30, 40, 50, 60]
-    num_expreriments = 1
-    num_epochs = 60
-    U_norms = 0
-    losses = 0
-    for i in range(num_expreriments):
-        U_norms_i, losses_i = train_and_evaluate(Ns=N, lrs=lr, num_neurons=num_neurons, num_layers=num_layers, num_epochs=num_epochs)
-        U_norms += U_norms_i
-        losses += losses_i
-    U_norms /= num_expreriments
-    losses /= num_expreriments
-    np.save('losses_nl_nn', losses)
-    plot_heatmap(U_norms, num_neurons, num_layers, rf'$L^2$ norm of error with N={N} and $\eta$ = {lr}', 'Number of hidden neurons', 'Number of hidden layers', 'beam_heatmap_num_neurons_layers80')
-    # exit()
-
     # N = 30
-    # lrs = [.05, .1, .5, .8]
-    # num_layers = [3, 4, 5, 6]
-    # num_neurons = 30
-    # num_expreriments = 10
+    # lr = .5
+    # num_layers = [2, 3, 4, 5]
+    # num_neurons = [30, 40, 50, 60]
+    # num_expreriments = 1
+    # num_epochs = 80
     # U_norms = 0
     # losses = 0
-    # st = time.time()
     # for i in range(num_expreriments):
-    #     U_norms_i, losses_i= train_and_evaluate(Ns=N, lrs=lrs, num_neurons=num_neurons, num_layers=num_layers, num_epochs=160)
+    #     U_norms_i, losses_i = train_and_evaluate(Ns=N, lrs=lr, num_neurons=num_neurons, num_layers=num_layers, num_epochs=num_epochs)
     #     U_norms += U_norms_i
     #     losses += losses_i
-    # print(time.time() - st)
     # U_norms /= num_expreriments
     # losses /= num_expreriments
-    # np.save('losses_lrs_nl', losses)
-    # plot_heatmap(U_norms, num_layers, lrs, rf'$L^2$ norm of error with N={N} and {num_neurons} hidden neurons', 'Number of layers', r'$\eta$', 'beam_heatmap_lrs_num_layers')
-    # # plot_heatmap(np.abs(e_norms), num_layers, lrs, rf'$L^2$ error norm with N={N} and {num_neurons} hidden neurons', 'Number of layers', r'$\eta$', 'beam_heatmap_lrs_num_layersABS')
+    # np.save('losses_nl_nn', losses)
+    # plot_heatmap(U_norms, num_neurons, num_layers, rf'$L^2$ norm of error with N={N} and $\eta$ = {lr}', 'Number of hidden neurons', 'Number of hidden layers', 'beam_heatmap_num_neurons_layers80')
+    # exit()
+
+    N = 20
+    lrs = [.05, .1, .5]
+    num_layers = [3, 4, 5]
+    num_neurons = 50
+    num_expreriments = 1
+    num_epochs = 80
+    U_norms = 0
+    losses = 0
+    st = time.time()
+    for i in range(num_expreriments):
+        U_norms_i, losses_i= train_and_evaluate(Ns=N, lrs=lrs, num_neurons=num_neurons, num_layers=num_layers, num_epochs=num_epochs)
+        U_norms += U_norms_i
+        losses += losses_i
+    print(time.time() - st)
+    U_norms /= num_expreriments
+    losses /= num_expreriments
+    np.save('losses_lrs_nl', losses)
+    plot_heatmap(U_norms, num_layers, lrs, rf'$L^2$ norm of error with N={N} and {num_neurons} hidden neurons', 'Number of layers', r'$\eta$', 'beam_heatmap_lrs_num_layers')
 
     # losses_lrs_nl = np.load(arrays_path / 'losses_lrs_nl.npy')
     # print(losses_lrs_nl)
     # losses_lrs_nl += np.abs(np.min(losses_lrs_nl[~np.isnan(losses_lrs_nl)]))
     # print(losses_lrs_nl)
     # print(np.min(losses_lrs_nl))
-
     # plot_losses(losses, [r'$\eta$ = ', lrs], ['# of hidden layers', num_layers], 2, 1, 40, [f'{num_layers[2]} hidden layers',rf'$\eta$ = {lrs[1]}'], 'beam_loss_lrs_nl')
 
     # N = 40
