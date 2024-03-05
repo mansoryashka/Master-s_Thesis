generate wrong results

3DBeam/DemBeam3D.py
@@ -47,7 +47,7 @@ d_boundary = 0.0
 d_cond = [0, 0, 0]
 
 n_boundary = L
-n_cond = [0, 0, 0]
+n_cond = [0, -5, 0]
 
 dir_bcs = {d_boundary: d_cond}
 neu_bcs = {n_boundary: n_cond}
@@ -339,10 +339,10 @@ if __name__ == '__main__':
     lrs = [.05, .1, .5, .9]
     num_layers = [3, 4, 5, 6]
     lrs = [.5]
-    num_layers = [6]
+    num_layers = [3]
     num_neurons = 50
     num_expreriments = 1
-    num_epochs = 300
+    num_epochs = 250
     U_norms = 0
     losses = 0
     st = time.time()
@@ -354,7 +354,7 @@ if __name__ == '__main__':
     U_norms /= num_expreriments
     losses /= num_expreriments
     np.save('losses_lrs_nl', losses)
-    plot_heatmap(U_norms, num_layers, lrs, rf'$L^2$ norm of error with N={N} and {num_neurons} hidden neurons', 'Number of layers', r'$\eta$', 'beam_heatmap_lrs_num_layers')
+    plot_heatmap(U_norms, num_layers, lrs, rf'$L^2$ norm of error with N={N} and {num_neurons} hidden neurons', 'Number of layers', r'$\eta$', 'beam_heatmap_lrs_num_layers1')
 
     # losses_lrs_nl = np.load(arrays_path / 'losses_lrs_nl.npy')
     # print(losses_lrs_nl)

DEM.py
@@ -66,13 +66,14 @@ class DeepEnergyMethod:
                 boundary_loss = torch.sum(bc_dir)
                 # external loss
                 neu_pred = self.getU(self.model, neuBC_coords)
-                bc_neu = torch.matmul((neu_pred + neuBC_coords).unsqueeze(1), neuBC_values.unsqueeze(2))
+                bc_neu = torch.bmm((neu_pred + neuBC_coords).unsqueeze(1), neuBC_values.unsqueeze(2))
                 
 
                 phi = u_pred
                 body_f = torch.matmul(phi.unsqueeze(1), fb.unsqueeze(2))
-                # external_loss = LHD[1]*LHD[2]*penalty(bc_neu) + LHD[0]*LHD[1]*LHD[2]*body_loss
-                external_loss = LHD[0]*LHD[1]*LHD[2]*penalty(body_f)
+                external_loss = LHD[1]*LHD[2]*penalty(bc_neu) #+ LHD[0]*LHD[1]*LHD[2]*body_loss
+                # external_loss = LHD[0]*LHD[1]*LHD[2]*penalty(body_f)
+
 
                 loss = internal_loss - external_loss + boundary_loss
 
@@ -84,8 +85,7 @@ class DeepEnergyMethod:
                 self.energy_loss = loss
 
                 self.current_loss = loss
-                #       + f'loss: {loss.item():10.5f}')
-                # print(f'Iter: {i+1:2d}, Energy: {loss}')
+                # print(f'Iter: {i:3d}, Energy: {self.energy_loss.item():10.5f}, Int: {self.internal_loss:10.5f}, Ext: {self.external_loss:10.5f}')
                 return loss
 
             optimizer.step(closure)