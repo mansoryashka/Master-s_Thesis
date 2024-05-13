fix energy calculation

3DBeam/DemBeam3D.py
@@ -43,7 +43,7 @@ dx = L/(4*N_test)
 dy = H/N_test
 dz = D/N_test
 
-dxdydz = [dx, dy, dz]
+# dxdydz = [dx, dy, dz]
 
 d_boundary = 0.0
 d_cond = [0, 0, 0]
@@ -83,7 +83,7 @@ def define_domain(L, H, D, N=25, dir_bcs=None, neu_bcs=None):
     ax.scatter(db_pts_x, db_pts_y, db_pts_z, facecolor='tab:green', s=0.5)
     ax.scatter(nb_pts_x, nb_pts_y, nb_pts_z, facecolor='tab:red', s=0.5)
     ax.set_box_aspect((4,1,1))
-    ax.set_xticks([0.0, 2.0, 4.0])
+    ax.set_xticks([0.0, 4.0])
     ax.set_zticks([0.0, 1.0])
     ax.set_yticks([0.0, 1.0])
     ax.set_xlabel('x')
@@ -226,7 +226,7 @@ def write_vtk_v2(filename, x_space, y_space, z_space, U):
 
 
 ### Skrive blokkene til en egen funksjon? Kalles på helt likt inne i loopene ###
-def train_and_evaluate(Ns=20, lrs=0.1, num_neurons=20, num_layers=2, num_epochs=40, max_it=20):
+def train_and_evaluate(Ns=20, lrs=0.1, num_neurons=20, num_layers=2, num_epochs=40, max_it=20, shape=[20, 20, 20]):
     num_losses = int(num_epochs / 5) + 1
     # train on many N values
     if isinstance((Ns and not lrs), (list, tuple)):
@@ -239,11 +239,13 @@ def train_and_evaluate(Ns=20, lrs=0.1, num_neurons=20, num_layers=2, num_epochs=
             DemBeam = DeepEnergyMethod(model, energy)
             domain, dirichlet, neumann = define_domain(L, H, D, N=N)
             # train model
-            DemBeam.train_model(domain, dirichlet, neumann, LHD, lr=lrs, max_it=max_it, epochs=num_epochs)
+            DemBeam.train_model(domain, dirichlet, neumann, shape, LHD, lr=lrs, max_it=max_it, epochs=num_epochs)
             # evaluate model
-            U_pred = DemBeam.evaluate_model(x, y, z)
+            U_pred, u_pred_torch, xyz_tensor = DemBeam.evaluate_model(x, y, z, return_pred_tensor=True)
+            VonMises_pred = VonMises_stress(u_pred_torch, xyz_tensor)
+            # store solution
+            write_vtk_v2(f'output/DemBeam_N{N}', x, y, z, U_pred)
             # calculate L2norm
-            # u_norms[i] = L2norm3D(np.transpose(U_pred, [0, 2, 1, 3]) - u_fem30, 4*N_test, N_test, N_test, dx, dy, dz)
             u_norms[i] = L2norm3D(U_pred - u_fem30, 4*N_test, N_test, N_test, dx, dy, dz)
             losses[:, i] = np.array(DemBeam.losses)
     # train on many learning rates and number of neurons in hidden layers
@@ -258,10 +260,12 @@ def train_and_evaluate(Ns=20, lrs=0.1, num_neurons=20, num_layers=2, num_epochs=
                 DemBeam = DeepEnergyMethod(model, energy)
                 domain, dirichlet, neumann = define_domain(L, H, D, N=Ns)
 
-                DemBeam.train_model(domain, dirichlet, neumann, LHD, lr=lr, max_it=max_it, epochs=num_epochs)
-                U_pred = DemBeam.evaluate_model(x, y, z)
+                DemBeam.train_model(domain, dirichlet, neumann, shape, LHD, lr=lr, max_it=max_it, epochs=num_epochs)
+                U_pred, u_pred_torch, xyz_tensor = DemBeam.evaluate_model(x, y, z, return_pred_tensor=True)
+                VonMises_pred = VonMises_stress(u_pred_torch, xyz_tensor)
 
-                # u_norms[i, j] = L2norm3D(np.transpose(U_pred, [0, 2, 1, 3]) - u_fem30, 4*N_test, N_test, N_test, dx, dy, dz)
+                # store solution
+                write_vtk_v2(f'output/DemBeam_lr{lr}_nn{n}', x, y, z, U_pred)
                 u_norms[i, j] = L2norm3D(U_pred - u_fem30, 4*N_test, N_test, N_test, dx, dy, dz)
                 losses[:, i, j] = np.array(DemBeam.losses)
     # train on many learning rates and number of hidden layers
@@ -275,12 +279,14 @@ def train_and_evaluate(Ns=20, lrs=0.1, num_neurons=20, num_layers=2, num_epochs=
                 model = MultiLayerNet(3, *([num_neurons]*l), 3)
                 DemBeam = DeepEnergyMethod(model, energy)
                 domain, dirichlet, neumann = define_domain(L, H, D, N=Ns)
-                DemBeam.train_model(domain, dirichlet, neumann, LHD, lr=lr, max_it=max_it, epochs=num_epochs)
+                DemBeam.train_model(domain, dirichlet, neumann, shape, LHD, lr=lr, max_it=max_it, epochs=num_epochs)
                 # evaluate model
-                U_pred = DemBeam.evaluate_model(x, y, z)
+                U_pred, u_pred_torch, xyz_tensor = DemBeam.evaluate_model(x, y, z, return_pred_tensor=True)
+                VonMises_pred = VonMises_stress(u_pred_torch, xyz_tensor)
+                # store solution
                 write_vtk_v2(f'output/DemBeam_lr{lr}_nl{l}', x, y, z, U_pred)
-                u_norms[i, j] = L2norm3D(np.transpose(U_pred, [0, 2, 1, 3]) - u_fem30, 4*N_test, N_test, N_test, dx, dy, dz)
-                # u_norms[i, j] = L2norm3D(U_pred - u_fem30, 4*N_test, N_test, N_test, dx, dy, dz)
+
+                u_norms[i, j] = L2norm3D(U_pred - u_fem30, 4*N_test, N_test, N_test, dx, dy, dz)
                 losses[:, i, j] = np.array(DemBeam.losses)
     # train on number of neurons in hidden layers and number of hidden layers
     # elif isinstance((num_neurons and num_layers), (list, tuple)):
@@ -294,12 +300,12 @@ def train_and_evaluate(Ns=20, lrs=0.1, num_neurons=20, num_layers=2, num_epochs=
                 model = MultiLayerNet(3, *([n]*l), 3)
                 DemBeam = DeepEnergyMethod(model, energy)
                 domain, dirichlet, neumann = define_domain(L, H, D, N=Ns)
-                DemBeam.train_model(domain, dirichlet, neumann, dxdydz, LHD, lr=lrs, max_it=max_it, epochs=num_epochs)
+                DemBeam.train_model(domain, dirichlet, neumann, shape, LHD, lr=lrs, max_it=max_it, epochs=num_epochs)
                 # evaluate model
                 U_pred, u_pred_torch, xyz_tensor = DemBeam.evaluate_model(x, y, z, return_pred_tensor=True)
                 VonMises_pred = VonMises_stress(u_pred_torch, xyz_tensor)
+                # store solution
                 write_vtk_v2(f'output/DemBeam_nn{n}_nl{l}', x, y, z, {'displacement': U_pred, 'VonMises stress': VonMises_pred})
-                # u_norms[i, j] = L2norm3D(np.transpose(U_pred, [0, 2, 1, 3]) - u_fem30, 4*N_test, N_test, N_test, dx, dy, dz)
                 u_norms[i, j] = L2norm3D(U_pred - u_fem30, 4*N_test, N_test, N_test, dx, dy, dz)
                 losses[:, i, j] = np.array(DemBeam.losses)
     # train on many N values and learning rates
@@ -316,11 +322,13 @@ def train_and_evaluate(Ns=20, lrs=0.1, num_neurons=20, num_layers=2, num_epochs=
                 DemBeam = DeepEnergyMethod(model, energy)
                 domain, dirichlet, neumann = define_domain(L, H, D, N=N)
                 # train model
-                DemBeam.train_model(domain, dirichlet, neumann, LHD, lr=lr, max_it=max_it, epochs=num_epochs)
+                DemBeam.train_model(domain, dirichlet, neumann, shape, LHD, lr=lr, max_it=max_it, epochs=num_epochs)
                 # evaluate model
-                U_pred = DemBeam.evaluate_model(x, y, z)
+                U_pred, u_pred_torch, xyz_tensor = DemBeam.evaluate_model(x, y, z, return_pred_tensor=True)
+                VonMises_pred = VonMises_stress(u_pred_torch, xyz_tensor)
+                # store solution
+                write_vtk_v2(f'output/DemBeam_lr{lr}_N{N}', x, y, z, {'displacement': U_pred, 'VonMises stress': VonMises_pred})
                 # calculate L2norm
-                # u_norms[i, j] = L2norm3D(np.transpose(U_pred, [0, 2, 1, 3]) - u_fem30, 4*N_test, N_test, N_test, dx, dy, dz)
                 u_norms[i, j] = L2norm3D(U_pred - u_fem30, 4*N_test, N_test, N_test, dx, dy, dz)
                 losses[:, i, j] = np.array(DemBeam.losses)
     else:
@@ -410,18 +418,18 @@ if __name__ == '__main__':
     y = np.linspace(0, D, N_test + 2)[1:-1]
     z = np.linspace(0, H, N_test + 2)[1:-1]
     start = time.time()
-    N = 30
+    N = 20
     lr = .5
-    # num_layers = [2, 3, 4, 5]
-    # num_neurons = [30, 40, 50, 60]
-    num_layers = [3, 4, 5]
-    num_neurons = [30, 40]
-    num_expreriments = 2
+
+    shape = [4*N, N, N]
+    num_layers = [2, 3, 4, 5]
+    num_neurons = [30, 40, 50, 60]
+    num_expreriments = 20
     num_epochs = 200
     U_norms = 0
     losses = 0
     for i in range(num_expreriments):
-        U_norms_i, losses_i = train_and_evaluate(Ns=N, lrs=lr, num_neurons=num_neurons, num_layers=num_layers, num_epochs=num_epochs)
+        U_norms_i, losses_i = train_and_evaluate(Ns=N, lrs=lr, num_neurons=num_neurons, num_layers=num_layers, num_epochs=num_epochs, shape=shape)
         U_norms += U_norms_i
         losses += losses_i
     U_norms /= num_expreriments

DEM.py
@@ -32,14 +32,20 @@ class DeepEnergyMethod:
         self.model = model.to(dev)
         self.energy = energy
         
-    def train_model(self, data, dirichlet, neumann, dxdydz, LHD, lr=0.5, max_it=20, epochs=20, fb=np.array([[0, -5, 0]])):
+    def train_model(self, data, dirichlet, neumann, shape, LHD, lr=0.5, max_it=20, epochs=20, fb=np.array([[0, -5, 0]])):
         # data
         # print(data)
-        N = np.array(LHD) / np.array(dxdydz)
-        N = list(map(int, N))
+        # N = np.array(LHD) / np.array(dxdydz)
+        # N = list(map(int, N))
+        dxdydz = np.array(LHD) / (np.array(shape) - 1)
+        # print(shape)
+        # print(LHD)
+        # print(dxdydz); exit()
+        # N = list(map(int, dxdydz))
 
         x = torch.from_numpy(data).float().to(dev)
         fb = torch.from_numpy(fb).float().to(dev)
+        # fb2 = fb*torch.ones((np.prod(shape), 3)).to(dev)
         x.requires_grad_(True)
         optimizer = torch.optim.LBFGS(self.model.parameters(), lr=lr, max_iter=max_it)
         self.x = x
@@ -64,7 +70,8 @@ class DeepEnergyMethod:
                 IntEnergy, J = self.energy(u_pred, x, J=True)
 
                 # internal_loss = LHD[0]*LHD[1]*LHD[2]*penalty(IntEnergy)
-                internal_loss = LHD[0]*LHD[1]*LHD[2]*simpsons3D(IntEnergy, dx=dxdydz[0], dy=dxdydz[1], dz=dxdydz[2], N=N[-1])
+                # print('internal')
+                internal_loss = simps3D(IntEnergy, dx=dxdydz[0], dy=dxdydz[1], dz=dxdydz[2], shape=shape)
 
                 # boundary loss
                 dir_pred = self.getU(self.model, dirBC_coords)
@@ -74,15 +81,16 @@ class DeepEnergyMethod:
                 # external loss
                 neu_pred = self.getU(self.model, neuBC_coords)
                 bc_neu = torch.bmm((neu_pred + neuBC_coords).unsqueeze(1), neuBC_values.unsqueeze(2))
-                
                 phi = u_pred
                 body_f = torch.matmul(phi.unsqueeze(1), fb.unsqueeze(2))
 
-                # print(body_f.shape)
+                # print(body_f.shape), exit()
                 # external_loss = LHD[0]*LHD[1]*LHD[2]*penalty(body_f)
-                external_loss = LHD[0]*LHD[1]*LHD[2]*simpsons3D(body_f, dx=dxdydz[0], dy=dxdydz[1], dz=dxdydz[2], N=N[-1])
+                # body_f = torch.bmm((u_pred + x).unsqueeze(1), fb2.unsqueeze(2))
+                # print('external')
+                external_loss = simps3D(body_f, dx=dxdydz[0], dy=dxdydz[1], dz=dxdydz[2], shape=shape)
 
-                loss = internal_loss - external_loss + boundary_loss
+                loss = internal_loss - external_loss# + boundary_loss
                 optimizer.zero_grad()
                 loss.backward()
 
@@ -168,23 +176,33 @@ def simpsons2D(U, x=None, y=None, dx=None, dy=None, N=25):
     elif (dx and dy):
         return simpson(simpson(U, dx=dy), dx=dx)
     
-def simpsons3D(U, x=None, y=None, z=None, dx=None, dy=None, dz=None, N=25):
-    Nx = 4*N; Ny = N; Nz = N
-    # U = U.cpu().detach().numpy()
-    # print(U.flatten().shape); exit()
+# def simpsons3D(U, x=None, y=None, z=None, dx=None, dy=None, dz=None, N=25):
+#     Nx = 4*N; Ny = N; Nz = N
+#     # U = U.cpu().detach().numpy()
+#     # print(U.flatten().shape); exit()
+#     U3D = U.flatten().reshape(Nx, Ny, Nz)
+#     # breakpoint()
+#     if (x and y and z):
+#         raise NotImplementedError('Not implemented yet. Please use dx and dy.')
+#     elif (dx and dy and dz):
+#         # print('U', U.shape); exit()
+#         # s1 = simpson(U, dx=dz)
+#         # s2 = simpson(s1, dx=dy)
+#         # s3 = simpson(s2, dx=dx)
+#         # print(U.shape)
+#         # print(s1.shape)
+#         # print(s2.shape)
+#         # print(s3)
+#         # return simpson(simpson(simpson(U, dx=dz), dx=dy), dx=dx)
+#         # print(dx)
+#         return simpson(simpson(simpson(U3D, dx=dz), dx=dy), dx=dx)
+    
+def simps3D(U, xyz=None, dx=None, dy=None, dz=None, shape=None):
+    # print('Mansur')
+    Nx = shape[0]
+    Ny = shape[1]
+    Nz = shape[2]
+    # print(U.shape)
     U3D = U.flatten().reshape(Nx, Ny, Nz)
-    # breakpoint()
-    if (x and y and z):
-        raise NotImplementedError('Not implemented yet. Please use dx and dy.')
-    elif (dx and dy and dz):
-        # print('U', U.shape); exit()
-        # s1 = simpson(U, dx=dz)
-        # s2 = simpson(s1, dx=dy)
-        # s3 = simpson(s2, dx=dx)
-        # print(U.shape)
-        # print(s1.shape)
-        # print(s2.shape)
-        # print(s3)
-        # return simpson(simpson(simpson(U, dx=dz), dx=dy), dx=dx)
-        # print(dx)
-        return simpson(simpson(simpson(U3D, dx=dz), dx=dy), dx=dx)
+    # print(U3D.shape)
+    return simpson(simpson(simpson(U3D, dx=dz), dx=dy), dx=dx)