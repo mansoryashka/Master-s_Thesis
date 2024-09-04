4a1e456da59b13c644fda99b2695f4949eda8d56
Mansur <mansurd@uio.no>
2024-09-04 11:40:40 +0200

    generate 1D figures

--- 1Dbar/FemBar1D.py
+++ 1Dbar/FemBar1D.py
@@ -67,26 +67,28 @@ if __name__ == '__main__':
         u = FEM_1D_Beam(N)
 
         us = np.array([u(xi) for xi in x])
-        abs_err = np.abs((us - u_ex)/u_ex)
+        abs_err = np.abs((us - u_ex))
         ax.semilogy(x, abs_err, label=f'N = {N}')
 
         ### Trolig bedre å ha i tabell
         du = np.gradient(us, dx)
-        abs_err2 = np.abs((du - du_ex)/du_ex)
+        abs_err2 = np.abs((du - du_ex))
         ax2.semilogy(x, abs_err2, label=f'N = {N}')
         # print(f'N: {N}, L2norm = {L2norm(us, u_ex)}')
         norms[N] = L2norm(us, u_ex, dx)
     
     np.save('u_fem', us)
 
-    ax.set_ylabel('Relative error of deformation')
     ax.set_xlabel('$X$')
-    ax2.set_ylabel('Relative error of deformation gradient')
-    ax2.set_xlabel('$X$')
+    ax.set_ylabel('Absolute error')
     ax.legend(loc='lower right')
+    # fig.savefig('figures/error_u_fem.pdf')
+
+    ax2.set_xlabel('$X$')
+    ax2.set_ylabel('Absolute error')
+    ax2.axhline(y=3e-5)
     ax2.legend(loc='lower right')
-    fig.savefig('figures/error_u_fem.pdf')
-    fig2.savefig('figures/error_du_fem.pdf')
+    # fig2.savefig('figures/error_du_fem.pdf')
 
     fig, ax = plt.subplots()
     ax.plot(x, u_ex, label='Exact')
@@ -94,17 +96,30 @@ if __name__ == '__main__':
     # plt.show()
     # fig.savefig("output/u.pdf")
 
-    ### et forsøk  på å regne ut konvergensrate? ###
+    Ns = np.asarray([10, 50, 100, 500, 1000, 5000, 10000])
+    e = np.zeros(Ns.shape)
+    for i, N in enumerate(Ns):
+        u = FEM_1D_Beam(N)
+        us = np.array([u(xi) for xi in x])
+
+        e[i] = L2norm(us, u_ex, dx)
+        print(L2norm(us, u_ex, dx))
+        print(e[i])
+
+    print(np.log(e[1:]/e[:-1])/np.log(Ns[1:]/Ns[:-1]))
+
     fig3, ax3 = plt.subplots()
-    prev_key=1
-    prev_val=1
-    for i, (key, val) in enumerate(norms.items()):
-        # print(key, prev_key)
-        print(np.log(prev_val/val)/np.log((1/prev_key)/(1/key)))
-        prev_key, prev_val = key, val
-        # print(f'{key:5d} & {val:8.3g}')
-    ax3.loglog(norms.keys(), norms.values(), '--o')
+    ax3.loglog(Ns, e, '--o')
     ax3.set_ylabel('Relative error of derformation')
     ax3.set_xlabel('N')
     fig3.savefig('figures/convergence.pdf')
+    # plt.show()
+    ### et forsøk  på å regne ut konvergensrate? ###
+    # prev_key=1
+    # prev_val=1
+    # for i, (key, val) in enumerate(norms.items()):
+    #     # print(key, prev_key)
+    #     print(np.log(prev_val/val)/np.log((1/prev_key)/(1/key)))
+    #     prev_key, prev_val = key, val
+    #     # print(f'{key:5d} & {val:8.3g}')
     # plt.show()
