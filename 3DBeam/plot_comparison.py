b991a8739e99fcbbc6ee6a312d9fdb479b4b077d
Mansur <mansurd@uio.no>
2024-09-11 10:06:32 +0200

    write script for plotting line in 3D beam

--- 3DBeam/plot_comparison.py
+++ 3DBeam/plot_comparison.py
@@ -1,9 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import torch
+from pathlib import Path
 
 import seaborn as sns
 sns.set()
+
 
 import sys
 sys.path.insert(0, "..")
@@ -11,12 +13,15 @@ from DEM import MultiLayerNet, L2norm3D, dev, write_vtk_v2, models_path, arrays_
 from EnergyModels import NeoHookeanEnergyModel
 from DemBeam import DeepEnergyMethodBeam, lmbda, mu, L, H, D
 
+import matplotlib
+matplotlib.rcParams['figure.dpi'] = 150
 if __name__ == '__main__':
     model = MultiLayerNet(3, *[50]*3, 3)
     energy = NeoHookeanEnergyModel(lmbda, mu)
     Dem_strain = DeepEnergyMethodBeam(model, energy)
 
     N_strain = 21
+    middle = int(np.floor(2*N_strain))
     x_strain = np.linspace(0, L, 4*N_strain + 2)[1:-1]
     y_strain = np.linspace(0, D, N_strain)
     z_strain = np.linspace(0, H, N_strain)
@@ -30,17 +35,26 @@ if __name__ == '__main__':
     y1_fem = Y_fem[c1]
     y2_fem = Y_fem[c2]
 
+    fig = plt.figure(figsize=(14, 8))
+    plt.style.use('seaborn-v0_8-darkgrid')
+    ax11 = plt.subplot2grid((2,3), (0,0), colspan=3, rowspan=1)
+    ax12 = plt.subplot2grid((2,3), (1,0))
+    ax13 = plt.subplot2grid((2,3), (1,1))
+    ax14 = plt.subplot2grid((2,3), (1,2))
 
-    fig1, ax1 = plt.subplots()
     fig2, ax2 = plt.subplots()
-    ax2.set_xscale('log')
+    ax11.plot(x_strain, y1_fem, label='FEM')
+    ax12.plot(x_strain[:2], y1_fem[:2], label='FEM')
+    ax13.plot(x_strain[middle-1:middle+2], y1_fem[middle-1:middle+2], label='FEM')
+    ax14.plot(x_strain[-2:], y1_fem[-2:], label='FEM')
 
-    # ax.plot(x_strain, y1_fem, label='FEM')
-    ax2.scatter(4*30*30*30, y2_fem, label='FEM')
-    # plt.show()
-    linestyles = [(5, (10, 3)), (0, (5, 10)), 'dashed', 'dotted']
+
+    ax2.axhline(y=y2_fem, label='FEM', linestyle='--', alpha=0.8)
+
+    colors = ['C1', 'C2', 'C3', 'yellow']
+
     for i, N in enumerate([30, 40, 50, 60]):
-        model_path = models_path / 'run4' / f'model_lr0.5_nn50_nl3_N{N}_0'
+        model_path = Path('trained_models') / 'run4' / f'model_lr0.5_nn50_nl3_N{N}_0'
         Dem_strain.model.load_state_dict(torch.load(model_path))
         U_pred = Dem_strain.evaluate_model(x_strain, y_strain, z_strain)
         # exit(np.asarray(U_pred).shape)
@@ -64,12 +78,40 @@ if __name__ == '__main__':
         y2_cur = Y_cur[c2]
         # z2_cur = Z_cur[c2]
 
-        # ax.plot(x_strain, y1_cur, linestyle=linestyles[i], alpha=0.9, label=f'N = {N}', linewidth=0.5)
-        ax1.semilogy(x_strain, np.abs(y1_fem - y1_cur), alpha=0.9, label=f'N = {N}', linewidth=1)
+        ax11.plot(x_strain, y1_cur, c=colors[i],
+                 linestyle='--', linewidth=0.8, 
+                 alpha=0.9, label=f'N = {N}')
+        ax12.plot(x_strain[:2], y1_cur[:2], c=colors[i],
+                 linestyle='--', linewidth=0.8, 
+                 alpha=0.9, label=f'N = {N}')
+        ax13.plot(x_strain[middle-1:middle+2], y1_cur[middle-1:middle+2], c=colors[i],
+                 linestyle='--', linewidth=0.8, 
+                 alpha=0.9, label=f'N = {N}')
+        ax14.plot(x_strain[-2:], y1_cur[-2:], c=colors[i],
+                 linestyle='--', linewidth=0.8, 
+                 alpha=0.9, label=f'N = {N}')
         ax2.scatter(4*N*N*N, y2_cur, c='tab:red')
-    ax1.legend()
+
+    ax11.set_xlabel('$x$ [m]')
+    ax11.set_ylabel('$y$-deflection [m]')
+    ax11.legend()
+
+    ax12.set_xlabel('$x$ [m]')
+    ax12.set_ylabel('$y$-deflection [m]')
+    ax12.set_ylim(bottom=y1_fem[1])
+    ax12.set_xlim(right=x_strain[1])
+
+    ax13.set_ylim((y1_fem[middle+1], y1_fem[middle-1]))
+    ax13.set_xlim((x_strain[middle-1], x_strain[middle+1]))
+    ax13.set_xlabel('$x$ [m]')
+
+    ax14.set_ylim(top=y1_fem[-2])
+    ax14.set_xlim(left=x_strain[-2])
+    ax14.set_xlabel('$x$ [m]')
+    
+    ax2.set_xscale('log')
     ax2.legend(['FEM', 'DEM'])
     plt.show()
-        # plot linjeendring
-        # plot enring i toppunkt
-        # hent tilsvarende resultater fra FEM
+    # plot linjeendring
+    # plot enring i toppunkt
+    # hent tilsvarende resultater fra FEM
